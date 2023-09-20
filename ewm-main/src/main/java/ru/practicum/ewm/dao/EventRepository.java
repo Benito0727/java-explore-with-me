8f60db9fc7c96fdbfb638ee763ec334bd46a0ae5
Benito <Benito0727@yandex.ru>
2023-09-20 21:11:15 +0300

    добавлены интерфейсы репозиториев

--- ewm-main/src/main/java/ru/practicum/ewm/dao/EventRepository.java
+++ ewm-main/src/main/java/ru/practicum/ewm/dao/EventRepository.java
@@ -2,9 +2,8 @@ package ru.practicum.ewm.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.practicum.ewm.model.entity.User;
-
+import ru.practicum.ewm.model.entity.Event;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface EventRepository extends JpaRepository<Event, String> {
 }
